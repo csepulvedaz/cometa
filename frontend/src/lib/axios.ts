087bf4feafea2b52596117ea232712d091a6fb43
Camilo J Sepulveda <csepulvedaz@unal.edu.co>
2024-02-15 19:47:33 -0300

    adding frontend basics

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export const createHeaders = () => {
+	return {
+		"Content-Type": "application/json",
+	};
+};
+
+export const createRequest = () => {
+	return axios.create({
+		baseURL: process.env.NEXT_PUBLIC_BACKEND_ENDPOINT,
+		headers: createHeaders(),
+	});
+};
+
+export const throwResErrors = (error: any) => {
+	let detail = error.response?.data?.detail;
+	if (detail instanceof Array) {
+		detail = {
+			internal: detail,
+			message: detail[0].msg,
+		};
+	}
+	console.error("Internal error", detail?.internal ?? detail.message ?? "Unknown");
+	throw detail;
+};
